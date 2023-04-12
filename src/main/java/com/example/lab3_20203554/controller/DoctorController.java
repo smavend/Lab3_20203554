b5d0efc6ef836946e8664428de4bde402a38c34d
amanr <amanrique068@gmail.com>
2023-04-11 22:01:02 -0500

    merge

--- src/main/java/com/example/lab3_20203554/controller/DoctorController.java
+++ src/main/java/com/example/lab3_20203554/controller/DoctorController.java
@@ -2,6 +2,8 @@ package com.example.lab3_20203554.controller;
 
 import com.example.lab3_20203554.repository.DoctorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DoctorController {
@@ -10,5 +12,8 @@ public class DoctorController {
     public DoctorController(DoctorRepository doctorRepository) {
         this.doctorRepository = doctorRepository;
     }
-    private
+    @GetMapping("/doctor")
+    private String buscarPorHospital(Model model){
+        a
+    }
 }
