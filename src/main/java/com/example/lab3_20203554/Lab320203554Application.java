c28e8ff95682c4dbe8fa3bff0cae99eea1e65b88
amanr <amanrique068@gmail.com>
2023-04-11 21:22:12 -0500

    first commit

---
+++ src/main/java/com/example/lab3_20203554/Lab320203554Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203554;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320203554Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320203554Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20203554/Lab320203554ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203554;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320203554ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
