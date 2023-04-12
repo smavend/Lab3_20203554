96e0c9ac10a229d3ad8c3976cacd7f4b691da46f
amanr <amanrique068@gmail.com>
2023-04-11 22:04:29 -0500

    adicional

--- src/main/java/com/example/lab3_20203554/controller/DoctorController.java
+++ src/main/java/com/example/lab3_20203554/controller/DoctorController.java
@@ -1,9 +1,13 @@
 package com.example.lab3_20203554.controller;
 
+import com.example.lab3_20203554.entity.Doctor;
 import com.example.lab3_20203554.repository.DoctorRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class DoctorController {
@@ -13,7 +17,10 @@ public class DoctorController {
         this.doctorRepository = doctorRepository;
     }
     @GetMapping("/doctor")
-    private String buscarPorHospital(Model model){
-        a
+    private String buscarPorHospital(@RequestParam("idhospital") int  idHospital,
+                                     Model model){
+        List<Doctor> lista = doctorRepository.buscarPorIdHospital(idHospital);
+        model.addAttribute("listaDoctores", lista);
+        return "listaDoctor";
     }
 }
