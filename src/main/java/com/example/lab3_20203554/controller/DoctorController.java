d67e11af05ced64793499482a09f51ba70bd785f
amanr <amanrique068@gmail.com>
2023-04-11 21:58:56 -0500

    ultimo

--- src/main/java/com/example/lab3_20203554/controller/DoctorController.java
+++ src/main/java/com/example/lab3_20203554/controller/DoctorController.java
@@ -1,4 +1,14 @@
 package com.example.lab3_20203554.controller;
 
+import com.example.lab3_20203554.repository.DoctorRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class DoctorController {
+    final DoctorRepository doctorRepository;
+
+    public DoctorController(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+    private
 }
